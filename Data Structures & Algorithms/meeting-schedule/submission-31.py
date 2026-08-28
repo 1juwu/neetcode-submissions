4330a1219d15c6b9d58b5505be1e8aba41b34ae8
吳易儒 <r12724053@ntu.edu.tw>
2026-08-28 16:51:08 +0800

    Add: meeting-schedule - submission-31

---
+++ Data Structures & Algorithms/meeting-schedule/submission-31.py
@@ -0,0 +1,28 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        
+        if not intervals:
+            return True
+        
+        # 1. 關鍵修正 1：主動指定用 Interval 的 start 屬性來進行排序
+        intervals.sort(key=lambda x: x.start)
+        # 拿進來一個物件（叫做 x），然後請去抓出這個物件的 .start（開始時間）屬性。
+        
+        # 2. 依序比較前後兩個會議
+        for i in range(len(intervals) - 1):
+            current_meeting = intervals[i]
+            next_meeting = intervals[i + 1]
+            
+            # 3.用 .end 和 .start 物件屬性拿取時間進行比較
+            if current_meeting.end > next_meeting.start:
+                return False 
+                
+        return True
