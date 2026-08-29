c5654787747e940715c4a08978bdfaa251e8a3b9
吳易儒 <r12724053@ntu.edu.tw>
2026-08-30 01:21:24 +0800

    Add: subtree-of-a-binary-tree - submission-27

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-27.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+        def serialize(node):
+            if not node:
+                return ",#"  # 用 '#' 代表空節點
+            # 格式：,當前值 + 左子樹字串 + 右子樹字串
+            return f",{node.val}" + serialize(node.left) + serialize(node.right)
+        
+        # 只要 subRoot 的字串出現在 root 的字串裡面，就是 True
+        return serialize(subRoot) in serialize(root)
+                
+                
+
+
+
+
+
